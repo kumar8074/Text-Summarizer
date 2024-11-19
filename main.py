325eee285590c0f40297d0f7ad7ffae8bf3d23cf
Lalan Kumar <21e51a6932@hitam.org>
2024-11-19 16:28:17 +0530

    data transformation completed

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.textSummarizer.logging import logger
 
 from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
-from src.textSummarizer.logging import logger
+from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
+
+
 
 STAGE_NAME="Data Ingestion stage"
 
@@ -9,6 +11,17 @@ try:
     logger.info(f"stage {STAGE_NAME} initiated")
     data_ingestion_pipeline=DataIngestionTrainingPipeline()
     data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"Stage {STAGE_NAME} completed")
+except Exception as e:
+    raise e
+
+
+STAGE_NAME="Data Transformation stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_transformation_pipeline=DataTransformationTrainingPipeline()
+    data_transformation_pipeline.initiate_data_transformation()
     logger.info(f"Stage {STAGE_NAME} completed")
 except Exception as e:
     raise e
