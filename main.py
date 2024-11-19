a538712049b634cd46bef10e9d22085901d35672
Lalan Kumar <21e51a6932@hitam.org>
2024-11-19 20:15:11 +0530

    model trainer completed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.textSummarizer.logging import logger
 
 from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
-
+from src.textSummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME="Data Ingestion stage"
@@ -25,3 +25,14 @@ try:
     logger.info(f"Stage {STAGE_NAME} completed")
 except Exception as e:
     raise e
+
+STAGE_NAME="Model Trainer stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    model_trainer_pipeline=ModelTrainerTrainingPipeline()
+    model_trainer_pipeline.initiate_model_trainer()
+    logger.info(f"Stage {STAGE_NAME} completed")
+except Exception as e:
+    raise e
+
