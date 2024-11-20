10b7fcc84538e95a32eadfdaa978e3ca3cbaf1c5
Lalan Kumar <21e51a6932@hitam.org>
2024-11-20 17:06:52 +0530

    model evaluation completed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.textSummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.textSummarizer.pipeline.stage_4_model_evaluation import ModelEvaluationTrainingPipeline
 
 
 STAGE_NAME="Data Ingestion stage"
@@ -32,6 +33,17 @@ try:
     logger.info(f"stage {STAGE_NAME} initiated")
     model_trainer_pipeline=ModelTrainerTrainingPipeline()
     model_trainer_pipeline.initiate_model_trainer()
+    logger.info(f"Stage {STAGE_NAME} completed")
+except Exception as e:
+    raise e
+
+
+STAGE_NAME="Model Evaluation stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    model_evaluation_pipeline=ModelEvaluationTrainingPipeline()
+    model_evaluation_pipeline.initiate_model_evaluation()
     logger.info(f"Stage {STAGE_NAME} completed")
 except Exception as e:
     raise e
